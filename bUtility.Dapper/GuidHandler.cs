a75a3350cd803d82b586c715b8b06e77aaad5f61
biks <biks@nbg.gr>
2016-02-02 08:49:26 +0200

    dapper utility

---
+++ bUtility.Dapper/GuidHandler.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUtility.Dapper
+{
+    public class GuidHandler : SqlMapper.TypeHandler<Guid>
+    {
+        public override Guid Parse(object value)
+        {
+            return new Guid((string)value);
+
+        }
+
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            parameter.Value = value.ToString();
+        }
+    }
+}
