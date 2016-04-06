bd6207ab0cf5696d28b0345b932e8346a43a0f78
Giorgos Marinos <biks@nbg.gr>
2016-04-06 12:54:27 +0300

    missing file

---
+++ bUtility.Sts.ApiClient/Controllers/IndexController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace bUtility.Sts.ApiClient.Controllers
+{
+    public class IndexController : ApiController
+    {
+        [HttpGet, HttpPost]
+        public HttpResponseMessage Get()
+        {
+            if (Request.Method == HttpMethod.Post)
+            {
+                var rresponse = Request.CreateResponse(HttpStatusCode.Redirect);
+                rresponse.Headers.Location = new Uri(Request.RequestUri.AbsoluteUri);
+                return rresponse;
+            }
+            var response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.OK;
+            response.Content = new StringContent("hi");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
---
+++ bUtility/RegistryExceptionLog.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUtility
+{
+    public class RegistryExceptionLog
+    {
+        string Source;
+        Func<Exception, string> ToJSON;
+        public RegistryExceptionLog( string source, Func<Exception, string> toJson)
+        {
+            Source = source;
+            ToJSON = toJson;
+        }
+
+
+        public void ToInfoLog(string message, EventLogEntryType logType = EventLogEntryType.Information)
+        {
+            try
+            {
+                EventLog.WriteEntry(Source,message,logType);
+            }
+            catch
+            {
+                throw new Exception("Failed to Log Message: " + message);
+            }
+        }
+        public void ToNotificationLog(string message, EventLogEntryType logType = EventLogEntryType.Warning)
+        {
+            try
+            {
+                EventLog.WriteEntry(Source, message, logType);
+            }
+            catch
+            {
+                throw new Exception("Failed to Log Message: " + message);
+            }
+        }
+        public void ToNotificationLog(Exception ex, EventLogEntryType logType = EventLogEntryType.Warning)
+        {
+            try
+            {
+                EventLog.WriteEntry( Source, ToJSON(ex), logType);
+            }
+            catch
+            {
+                ToEventLog(ex.Message, EventLogEntryType.Error);
+                throw new Exception("Error Logging Exception", ex);
+            }
+        }
+        public void ToNotificationLog(Exception ex, string message, EventLogEntryType logType = EventLogEntryType.Error)
+        {
+            try
+            {
+                EventLog.WriteEntry( Source, message + ": " + ToJSON(ex), logType);
+            }
+            catch
+            {
+                ToEventLog(ex.Message, EventLogEntryType.Error);
+                throw new Exception("Error Logging Exception with message: " + message, ex);
+            }
+        }
+
+        public void ToEventLog(string message, EventLogEntryType logType = EventLogEntryType.Error)
+        {
+            try
+            {
+                EventLog.WriteEntry( Source, message, logType);
+            }
+            catch
+            {
+                throw new Exception("Failed to Log Message: " + message);
+            }
+        }
+        public void ToEventLog(Exception ex, EventLogEntryType logType = EventLogEntryType.Error)
+        {
+            try
+            {
+                EventLog.WriteEntry( Source, ToJSON(ex), logType);
+            }
+            catch
+            {
+                ToEventLog(ex.Message, EventLogEntryType.Error);
+                throw new Exception("Error Logging Exception", ex);
+            }
+        }
+        public void ToEventLog(Exception ex, string message, EventLogEntryType logType = EventLogEntryType.Error)
+        {
+            try
+            {
+                EventLog.WriteEntry( Source, message + ": " + ToJSON(ex), logType);
+            }
+            catch
+            {
+                ToEventLog(ex.Message, EventLogEntryType.Error);
+                throw new Exception("Error Logging Exception with message: " + message, ex);
+            }
+        }
+    }
+}
