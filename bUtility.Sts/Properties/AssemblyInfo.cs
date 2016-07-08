fc41a092ae249f0c82a53feafbdbd4b57c14faf7
biks <biks@nbg.gr>
2016-07-08 07:09:55 +0300

    butility.sts package

--- bUtility.Sts/Properties/AssemblyInfo.cs
+++ bUtility.Sts/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("bUtility.Sts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("STS Utility Library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("biks")]
 [assembly: AssemblyProduct("bUtility.Sts")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
