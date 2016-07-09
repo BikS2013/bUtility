53592864b7552582e0eac3d3f9ef138ffa5422dd
biks <biks@nbg.gr>
2016-07-09 11:12:47 +0300

    +logging test

---
+++ bUtility.Logging.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bUtility.Logging.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEventViewer()
+        {
+            var l = new Logger("bUtilitySource");
+            l.Error("test logger 1");
+
+            l.Error(new Exception("an exception sample"));
+        }
+    }
+}
