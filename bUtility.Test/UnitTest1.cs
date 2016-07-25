1856112545b57738fdfae0d992bbf8995917ca60
Giorgos Marinos <biks@nbg.gr>
2016-07-25 21:54:03 +0300

    some test

---
+++ bUtility.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using bUtility.Reflection;
+
+namespace bUtility.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t = new testClass
+            {
+                channel = "ch1",
+                username = "usr1",
+                password = "psv1",
+                sessionId = Guid.NewGuid().ToString()
+            };
+            var fields = new string[] { "username", "password", "sessionId", "channel" };
+
+            Dictionary<string, string> rqst = new Dictionary<string, string>();
+            var sid = t.GetValue<string>("sessionId", null);
+            fields.ToList().ForEach(f => rqst[f] = t.GetValue<string>(f, null));
+
+        }
+    }
+}
---
+++ bUtility.Test/testClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUtility.Test
+{
+    public class testClass
+    {
+        public string sessionId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string channel { get; set; }
+    }
+}
