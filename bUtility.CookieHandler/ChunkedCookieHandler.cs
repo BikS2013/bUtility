4781ce63c8ec806b4ac8a7954b4c04fad19f48f9
Giorgos Marinos <biks@nbg.gr>
2016-09-09 21:44:17 +0300

    +bUtility.CookieHanler+SecureChunkedCookieHandler

--- bUtility.CookieHandler/ChunkedCookieHandler.cs
+++ bUtility.CookieHandler/ChunkedCookieHandler.cs
@@ -10,7 +10,7 @@ using System.Web;
 
 namespace bUtility.CookieHandler
 {
-    public sealed class ChunkedCookieHandler : System.IdentityModel.Services.CookieHandler
+    public abstract class ChunkedCookieHandler : System.IdentityModel.Services.CookieHandler
     {
         public const int DefaultChunkSize = 2000;
 
@@ -80,7 +80,7 @@ namespace bUtility.CookieHandler
         }
 
 
-        private IEnumerable<KeyValuePair<string, string>> GetCookieChunks(string baseName, string cookieValue)
+        internal IEnumerable<KeyValuePair<string, string>> GetCookieChunks(string baseName, string cookieValue)
         {
             int num = CeilingDivide(cookieValue.Length, this._chunkSize);
             //if (num > 20 && DiagnosticUtility.ShouldTrace(TraceEventType.Warning))
@@ -158,36 +158,37 @@ namespace bUtility.CookieHandler
             this.WriteInternal(value, name, path, domain, expirationTime, secure, httpOnly, context.Request.Cookies, context.Response.Cookies);
         }
 
-        internal void WriteInternal(byte[] value, string name, string path, string domain, DateTime expirationTime, bool secure, bool httpOnly, HttpCookieCollection requestCookies, HttpCookieCollection responseCookies)
-        {
-            string cookieValue = Convert.ToBase64String(value);
-            this.DeleteInternal(name, path, domain, requestCookies, responseCookies);
-            foreach (KeyValuePair<string, string> current in this.GetCookieChunks(name, cookieValue))
-            {
-                HttpCookie httpCookie = new HttpCookie(current.Key, current.Value);
-                httpCookie.Secure = secure;
-                httpCookie.HttpOnly = httpOnly;
-                httpCookie.Path = path;
-                if (!string.IsNullOrEmpty(domain))
-                {
-                    httpCookie.Domain = domain;
-                }
-                if (expirationTime != DateTime.MinValue)
-                {
-                    httpCookie.Expires = expirationTime;
-                }
-                responseCookies.Set(httpCookie);
-                //if (DiagnosticUtility.ShouldTrace(TraceEventType.Information))
-                //{
-                //    TraceEventType arg_A6_0 = TraceEventType.Information;
-                //    int arg_A6_1 = 786438;
-                //    string arg_A6_2 = null;
-                //    ChunkedCookieHandlerTraceRecord.Action arg_A0_0 = ChunkedCookieHandlerTraceRecord.Action.Writing;
-                //    HttpCookie expr_9A = httpCookie;
-                //    TraceUtility.TraceEvent(arg_A6_0, arg_A6_1, arg_A6_2, new ChunkedCookieHandlerTraceRecord(arg_A0_0, expr_9A, expr_9A.Path), null);
-                //}
-            }
-        }
+        internal abstract void WriteInternal(byte[] value, string name, string path, string domain, DateTime expirationTime, bool secure, bool httpOnly, HttpCookieCollection requestCookies, HttpCookieCollection responseCookies);
+        //internal void WriteInternal(byte[] value, string name, string path, string domain, DateTime expirationTime, bool secure, bool httpOnly, HttpCookieCollection requestCookies, HttpCookieCollection responseCookies)
+        //{
+        //    string cookieValue = Convert.ToBase64String(value);
+        //    this.DeleteInternal(name, path, domain, requestCookies, responseCookies);
+        //    foreach (KeyValuePair<string, string> current in this.GetCookieChunks(name, cookieValue))
+        //    {
+        //        HttpCookie httpCookie = new HttpCookie(current.Key, current.Value);
+        //        httpCookie.Secure = secure;
+        //        httpCookie.HttpOnly = httpOnly;
+        //        httpCookie.Path = path;
+        //        if (!string.IsNullOrEmpty(domain))
+        //        {
+        //            httpCookie.Domain = domain;
+        //        }
+        //        if (expirationTime != DateTime.MinValue)
+        //        {
+        //            httpCookie.Expires = expirationTime;
+        //        }
+        //        responseCookies.Set(httpCookie);
+        //        //if (DiagnosticUtility.ShouldTrace(TraceEventType.Information))
+        //        //{
+        //        //    TraceEventType arg_A6_0 = TraceEventType.Information;
+        //        //    int arg_A6_1 = 786438;
+        //        //    string arg_A6_2 = null;
+        //        //    ChunkedCookieHandlerTraceRecord.Action arg_A0_0 = ChunkedCookieHandlerTraceRecord.Action.Writing;
+        //        //    HttpCookie expr_9A = httpCookie;
+        //        //    TraceUtility.TraceEvent(arg_A6_0, arg_A6_1, arg_A6_2, new ChunkedCookieHandlerTraceRecord(arg_A0_0, expr_9A, expr_9A.Path), null);
+        //        //}
+        //    }
+        //}
 
         private static string GetChunkName(string baseName, int chunkIndex)
         {
---
+++ bUtility.CookieHandler/SecureChunkedCookieHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace bUtility.CookieHandler
+{
+    public sealed class SecureChunkedCookieHandler: ChunkedCookieHandler
+    {
+        internal override void WriteInternal(byte[] value, string name, string path, string domain, DateTime expirationTime, bool secure, bool httpOnly, HttpCookieCollection requestCookies, HttpCookieCollection responseCookies)
+        {
+            string cookieValue = Convert.ToBase64String(value);
+            this.DeleteInternal(name, path, domain, requestCookies, responseCookies);
+            foreach (KeyValuePair<string, string> current in this.GetCookieChunks(name, cookieValue))
+            {
+                HttpCookie httpCookie = new HttpCookie(current.Key, current.Value);
+                httpCookie.Secure = true;
+                httpCookie.HttpOnly = httpOnly;
+                httpCookie.Path = path;
+                if (!string.IsNullOrEmpty(domain))
+                {
+                    httpCookie.Domain = domain;
+                }
+                if (expirationTime != DateTime.MinValue)
+                {
+                    httpCookie.Expires = expirationTime;
+                }
+                responseCookies.Set(httpCookie);
+                //if (DiagnosticUtility.ShouldTrace(TraceEventType.Information))
+                //{
+                //    TraceEventType arg_A6_0 = TraceEventType.Information;
+                //    int arg_A6_1 = 786438;
+                //    string arg_A6_2 = null;
+                //    ChunkedCookieHandlerTraceRecord.Action arg_A0_0 = ChunkedCookieHandlerTraceRecord.Action.Writing;
+                //    HttpCookie expr_9A = httpCookie;
+                //    TraceUtility.TraceEvent(arg_A6_0, arg_A6_1, arg_A6_2, new ChunkedCookieHandlerTraceRecord(arg_A0_0, expr_9A, expr_9A.Path), null);
+                //}
+            }
+        }
+
+    }
+}
