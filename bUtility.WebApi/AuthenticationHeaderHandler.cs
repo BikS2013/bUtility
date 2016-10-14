b6ef2bc5a0e6837da091733f345a371ec4612e65
biks <biks@nbg.gr>
2016-10-15 00:49:49 +0300

    +handlers

---
+++ bUtility.WebApi/AuthenticationHeaderHandler.cs
@@ -0,0 +1,46 @@
+﻿using bUtility.Logging;
+using bUtility.SecurityToken;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace bUtility.WebApi
+{
+    public class AuthenticationHeaderHandler : DelegatingHandler
+    {
+        ILogger Logger;
+        public AuthenticationHeaderHandler(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            try
+            {
+                var principal = request.Headers.Authorization.GetClaimsPrincipal("SAML");
+
+                if (principal?.Identity?.IsAuthenticated == true)
+                {
+                    Thread.CurrentPrincipal = principal;
+                    if (HttpContext.Current != null)
+                    {
+                        HttpContext.Current.User = principal;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
---
+++ bUtility.WebApi/RequiresAuthenticationHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bUtility.WebApi
+{
+    public class RequiresAuthenticationHandler : DelegatingHandler
+    {
+        public RequiresAuthenticationHandler()
+        {
+        }
+
+        //http://www.asp.net/web-api/overview/security/authentication-and-authorization-in-aspnet-web-api
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            if (Thread.CurrentPrincipal?.Identity?.IsAuthenticated != true)
+            {
+                return SendUnauthorizedResponse(request);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        private Task<HttpResponseMessage> SendUnauthorizedResponse(HttpRequestMessage request)
+        {
+            var unauthorizedResponse = request.CreateResponse(HttpStatusCode.Forbidden);
+
+            unauthorizedResponse.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue("SAML", ""));
+
+            unauthorizedResponse.ReasonPhrase = "Forbidden.";
+
+            return Task.FromResult(unauthorizedResponse);
+        }
+    }
+}
