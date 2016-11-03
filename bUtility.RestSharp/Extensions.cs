3e2d3ca0cf64a1085af3004ffe090b65afc26a5a
Giorgos Marinos <biks@nbg.gr>
2016-11-03 16:32:18 +0200

    + bUtility.RestSharp

---
+++ bUtility.RestSharp/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUtility.RestSharp
+{
+    public static class Extensions
+    {
+        public static IRestResponse<R> ExecutePost<T, R>(this RestClient client, RestRequest request, T requestData, string address) where R : new()
+        {
+            var httpRequest = request ?? new RestRequest(address, Method.POST);
+            httpRequest.AddHeader("Accept", "application/json");
+            httpRequest.AddHeader("Content-type", "application/json");
+
+            httpRequest.AddJsonBody(requestData);
+            IRestResponse<R> resp = client.Execute<R>(httpRequest);
+            return resp;
+        }
+
+
+    }
+}
