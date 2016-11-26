ce835586a7bb6cb6506dc56e79133b0da5fc59d0
Giorgos Marinos <biks@nbg.gr>
2016-11-26 21:31:30 +0200

    +bUtility.Json.Test

---
+++ bUtility.Json.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bUtility.Json.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGenericDataContractResolver()
+        {
+            Case1 c1 = new Case1 { UserName = "user", Password = "password" };
+            var text1 = Newtonsoft.Json.JsonConvert.SerializeObject(c1);
+
+            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
+            settings.ContractResolver = new GenericDataContractResolver(new string[] { "password" });
+            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            var text2 = Newtonsoft.Json.JsonConvert.SerializeObject(c1, settings);
+
+            var c2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Case1>(text1, settings);
+        }
+    }
+}
