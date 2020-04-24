98f0437160b49fd342c894c42b6c6977e101256d
helnik <chelmis.nikolaos@nbg.gr>
2020-04-24 11:06:21 +0300

    Revert "Change HttpClient usage"
    This reverts commit 9352f0a099f61fda20482ed6f58ecec3fcbe8ab2.

--- bUtility.ReverseProxy/Types/ReverseProxyConfigList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace bUtility.ReverseProxy
-{
-    public class ReverseProxyConfigList
-    {
-        public List<ReverseProxyConfig> ReverseProxyConfiguration { get; set; }
-    }
-}
--- bUtility.ReverseProxy/Types/RouteConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace bUtility.ReverseProxy
-{
-    public class RouteConfig
-    {
-        public string RouteName { get; set; }
-        public string RouteTemplate { get; set; }
-    }
-}
